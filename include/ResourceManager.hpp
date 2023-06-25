9bed113733424cfe16cd385367af12f2289decc0
Reinout Nonhebel <github@rpnonhebel.nl>
2023-06-25 12:24:18 +0200

    Add missing default destructor
    new/delete type mismatch found by address sanitizer!

--- include/ResourceManager.hpp
+++ include/ResourceManager.hpp
@@ -7,6 +7,8 @@ template <typename T>
 class ResourceManager
 {
 public:
+    virtual ~ResourceManager() = default;
+
     virtual void registerResource(const std::string &key, const std::string &filename) = 0;
     virtual std::shared_ptr<T> getResource(const std::string &key) = 0;
 };
