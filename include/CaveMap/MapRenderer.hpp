6df3ee3ed5d9f1fc20f4f23042882fa66459c40b
Reinout Nonhebel <github@rpnonhebel.nl>
2025-06-21 11:41:24 +0200

    WIP add TileRenderInfo class

--- include/CaveMap/MapRenderer.hpp
+++ include/CaveMap/MapRenderer.hpp
@@ -4,6 +4,15 @@
 #include "Tile.hpp"
 #include "Grid2D.hpp"
 
+struct TileRenderInfo
+{
+    GridCoordinate position;
+    bool visible;
+    
+    std::shared_ptr<sf::Texture> texture;
+    int rotation;
+};
+
 class MapRenderer
 {
 public:
